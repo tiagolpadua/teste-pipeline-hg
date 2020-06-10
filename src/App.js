7cf3d8695b9e7745eacceb5f268e7ba746a795e1
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-06-10 15:31:41 -0300

    quebrando

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprenda React
         </a>
         <span>Very Cool</span>
       </header>
